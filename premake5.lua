821dd46a6841d5fddd77e179a11578a92b49e2a1
Dheeraj Karki <dheerajkarki1790@outlook.com>
2019-05-05 21:46:27 +0100

    Refactor and correcting indentation of premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,33 +1,25 @@
 project "ImGui"
     kind "StaticLib"
     language "C++"
-
+    cppdialect "C++17"
+    staticruntime "on"
+    systemversion "latest"
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
     files
     {
-    "imconfig.h",
-    "imstb_rectpack.h",
-    "imgui.h",
-    "imgui_internal.h",
-    "imstb_textedit.h",
-    "imstb_truetype.h",
-    "imgui_demo.cpp",
-    "imgui_draw.cpp",
-    "imgui_widgets.cpp",
-    "imgui.cpp"
+        "imconfig.h",
+        "imstb_rectpack.h",
+        "imgui.h",
+        "imgui_internal.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_widgets.cpp",
+        "imgui.cpp"
     }
-
-    filter "system:windows"
-        cppdialect "C++17"
-        staticruntime "on"
-        systemversion "latest"
-
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
-
-    filter "system:macosx"
-        cppdialect "C++17"
-        staticruntime "on"
-        systemversion "latest"
+    --Configuration specific settings for all system
+    filter "configurations:Release"
+        runtime "Release"
